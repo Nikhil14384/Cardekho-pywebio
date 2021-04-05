b9eedea92b355689ff87e0ce4d850d796df39d8e
Nareddula Nikhil Kumar Reddy <36074690+Nikhil14384@users.noreply.github.com>
2021-04-05 22:37:53 +0530

    Update app1.py

--- app1.py
+++ app1.py
@@ -89,7 +89,7 @@ def hitman():
         put_text("Oops...! you cannot sell your car")
     else:
         put_text('The resale value of your car is : %.1f lakhs ' %(output))
-    app.add_url_rule('/hitman', 'webio_view', webio_view(hitman),
+    app1.add_url_rule('/hitman', 'webio_view', webio_view(hitman),
             methods=['GET', 'POST', 'OPTIONS'])
     
     
